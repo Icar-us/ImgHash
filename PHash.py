58232389edbab381b4cd56d326ebafdd0bead6bb
sol-a-r <Icarus_fell@protonmail.com>
2018-04-07 02:35:14 -0500

    lol

--- PHash.py
+++ PHash.py
@@ -1,3 +1,29 @@
+
+#imports
+import os
+import sys
+from difflib import SequenceMatcher
+from PIL import Image
 import imagehash
-phash_simple(pics/doggo.jpg, hash_size=8, highfreq_factor=4)
-sleep(5)
+
+#defs
+def similar(a, b):
+     print ( SequenceMatcher(None, a, b).ratio() )
+
+#path = '/home/solar45/Documents/Github/ImgHash/pics/'   #define PATH to dir to be checked
+
+path = '/home/solar45/Documents/Github/ImgHash/pics/'
+
+l = []
+
+for filename in os.listdir(path):   #run through PATH and get names
+    print(filename)
+    hash = imagehash.average_hash(Image.open(path+filename))    #Hash files in PATH to be able to compare them
+    l.append(hash)
+pass
+
+
+
+img1 = str(l[0])
+img2 = str(l[1])
+similar(img1,img2)
---
+++ append.py
@@ -0,0 +1,8 @@
+l = []
+x=0
+while x<6:
+    x=x+1
+    l.append(x)
+
+    pass
+print(l)

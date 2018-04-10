b59dbf34b95adb5fc41f2a33155192d47aa4b767
sol-a-r <Icarus_fell@protonmail.com>
2018-04-09 19:54:28 -0500

    update

--- PHash_rename.py
+++ PHash_rename.py
@@ -10,20 +10,19 @@ import imagehash
 def similar(a, b):
      print ( SequenceMatcher(None, a, b).ratio() )
 
-#path = '/home/solar45/Documents/Github/ImgHash/pics/'   #define PATH to dir to be checked
+path = '/home/solar45/Pictures/MEGAsync/Edits/'   #define PATH to dir to be checked
 
-path = '/home/solar45/Desktop/Edits/'
 
-l = []
 
 for filename in os.listdir(path):   #run through PATH and get names
     print(filename)
     hash = imagehash.average_hash(Image.open(path+filename))    #Hash files in PATH to be able to compare them
-    l.append(hash)
+    print(hash)    
+
+
+    src = path+filename
+    dst = path+str(hash)
+
+    os.rename(src, dst)
 pass
 
-
-
-img1 = str(l[0])
-img2 = str(l[1])
-similar(img1,img2)
